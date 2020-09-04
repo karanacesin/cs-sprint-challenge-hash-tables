94ff52a9ede598de42a00c5e1b09106ffa98d7aa
Dawna Backmeier <karanacesin@yahoo.com>
2020-09-04 12:43:15 -0400

    mvp

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,16 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+    result = []
+
+    for x in a:
+        if abs(x) in cache:
+            result.append(abs(x))
+
+        else:
+            cache[abs(x)] = 1
+
 
     return result
 
